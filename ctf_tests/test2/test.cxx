69c0086d580e2daf5c63d3e7a6377ed64c4fe519
Andreas V Copan <avcopan@gmail.com>
2015-08-31 12:43:48 -0400

    More CTF test -- just learning how to initialize all of the basic data types

--- ctf_tests/test2/test.cxx
+++ ctf_tests/test2/test.cxx
@@ -10,8 +10,22 @@ int main(int argc, char** argv)
   enum SYM : int { NS, SY, AS, SH };
 
   {CTF::World world(argc, argv);
-    CTF::Scalar<> scalar(1.0, world);
-    CTF::Matrix<> matrix(10, 10, SYM::SY, world);
+    CTF::Scalar<> scalar(world);
+    CTF::Vector<> vector(3, world);
+    CTF::Matrix<> matrix(3, 3, SYM::NS, world);
+    CTF::Matrix<> symmat(3, 3, SYM::SY, world);
+    int dim1[] = {3,3,3};
+    int sym1[] = {SYM::NS, SYM::NS, SYM::NS};
+    CTF::Tensor<> tensor(3, dim1, sym1, world);
+    int dim2[] = {3,3,3,3};
+    int sym2[] = {SYM::AS, SYM::NS, SYM::AS, SYM::NS};
+    CTF::Tensor<> asmten(4, dim2, sym2, world);
+    scalar.print(stdout);
+    vector.print(stdout);
+    matrix.print(stdout);
+    symmat.print(stdout);
+    tensor.print(stdout);
+    asmten.print(stdout);
   }
   
 }
